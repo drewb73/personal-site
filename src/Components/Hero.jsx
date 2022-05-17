da6e63811da26ec03c55ab3fcb7494dbf2cf5d65
drewb73 <bartondrew5@gmail.com>
2022-05-16 20:04:43 -0700

    Pic and Home Update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,17 @@
 
-
 import React from 'react'
+
 
 function Hero() {
   return (
     <div class="hero max-h-screen bg-base-100">
   <div class="hero-content text-center">
     <div class="max-w-md">
+    <div class="avatar">
+  <div className='rounded-full mb-5 shadow-xl border-solid'>
+    <img src="/profilepic.jpg" alt='pic' />
+  </div>
+</div>
       <h1 class="text-5xl font-bold">Welcome!</h1>
       <p class="py-6">Hello, My name is Drew Barton I am a Sales Operations Professional in the tech industry. This site is my personal project I am in no way a pro web developer but please enjoy!</p>
       <button class="mx-2 btn btn-ghost">Linkedin</button>
