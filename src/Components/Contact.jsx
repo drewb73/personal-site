4ebef258e4273a7fd5c9017e6d8ca985eee75c90
drewb73 <bartondrew5@gmail.com>
2022-05-19 13:20:24 -0700

    Contact Form updated

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,10 +15,11 @@ function Contact() {
   return (
     <div className='container text-center mt-5'>
         <h1 className='text-center text-2xl font-bold mt-8'>Contact Me!</h1>
-        <form className='w-full'>
-         <input type='text' placeholder='Subject' value={subject} onChange={handleChange} className='input input-bordered input-md w-full  mt-3' />
+        <form className='w-auto'>
+         <input type='text' placeholder='Subject' value={subject} onChange={handleChange} className='input input-bordered input-md mt-3 w-96' />
          <br></br>
-         <textarea value={message} onChange={onChange} className='textarea textarea-primary mt-3 w-full' ></textarea>
+         <textarea value={message} onChange={onChange} className='textarea textarea-primary mt-3 w-96 justify-center' ></textarea>
+         <br></br>
          <a href={`mailto:${data.email}?Subject=${subject}&body=${message}`}><button className='btn btn-primary' type='button'>Send Email</button></a>
         </form>
     </div>
