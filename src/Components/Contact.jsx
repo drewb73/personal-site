5e28bc7c6102e9bc45af19993a9ac3a0eb0ecf26
drewb73 <bartondrew5@gmail.com>
2022-05-20 09:34:51 -0700

    Refactored State in Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 
 
 function Contact() {
-    const [message, setMessage] = useState('')
-    const [subject, setSubject] = useState('')
+    const [formData, setFormData] =useState({
+      message:'',
+      subject:''
+    })
 
-    const onChange = e => setMessage(e.target.value)
-    const handleChange = e => setSubject(e.target.value)
+    const {message, subject} = formData
 
+    const onChange = (e) => {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value
+      }))
+    }
+    
     const data = {
         email: 'bartondrew5@gmail.com',
     }
@@ -16,9 +24,9 @@ function Contact() {
     <div className='container text-center mt-5'>
         <h1 className='text-center text-2xl font-bold mt-8'>Contact Me!</h1>
         <form className='w-auto'>
-         <input type='text' placeholder='Subject' value={subject} onChange={handleChange} className='input input-bordered input-md mt-3 w-96' />
+         <input type='text' placeholder='Subject' value={subject} name='subject' onChange={onChange} required className='input input-bordered input-md mt-3 w-96' />
          <br></br>
-         <textarea placeholder='Message' value={message} onChange={onChange} className='textarea textarea-bordered mt-3 w-96 justify-center' ></textarea>
+         <textarea placeholder='Message' value={message} name='message' onChange={onChange} required className='textarea textarea-bordered mt-3 w-96 justify-center' ></textarea>
          <br></br>
          <a href={`mailto:${data.email}?Subject=${subject}&body=${message}`}><button className='btn btn-primary' type='button'>Send Email</button></a>
         </form>
