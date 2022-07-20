739ec361fb20db762a32e7510f44f10bad779a23
drewb73 <bartondrew5@gmail.com>
2022-07-20 08:04:25 -0700

    Update

--- src/Components/CardCarousel.jsx
+++ src/Components/CardCarousel.jsx
@@ -43,12 +43,12 @@ function CardCarousel() {
           </div>
           <div id='item4' className='carousel-item relative w-full justify-center'>
               <div className='card w-96 bg-sky-200 shadow-xl'>
-                  <figure><img src='/MERNapp.PNG' alt='me'></img></figure>
+                  <figure><img src='/openReq.PNG' alt='me'></img></figure>
                   <div className='card-body items-center text-center'>
-                      <h2 className='card-title'>Full Stack Blog App</h2>
-                      <p>In progress MERN stack App, stay tuned!</p>
+                      <h2 className='card-title'>MERN Stack Job Tracker</h2>
+                      <p>A full stack app to track the job search process</p>
                       <div className='card-actions justify-end'>
-                          <a href='https://github.com/drewb73/blog' className='btn btn-primary'>Project Here!</a>
+                          <a href='https://github.com/drewb73/client-jobify' className='btn btn-primary'>Project Here!</a>
                       </div>
                   </div>
               </div>
